9e17b6d410a10faf6c5e2f6e87652acf777e50d2
binnnfn <165402118+binnnfn@users.noreply.github.com>
2024-04-02 21:21:28 +0300

    Add files via upload

---
+++ js/layout.js
@@ -0,0 +1,11 @@
+(function($){
+	var initLayout = function() {
+
+
+		$('#myGallery').spacegallery({loadingClass: 'loading',duration:2000});
+	};
+	
+	
+	EYE.register(initLayout, 'init');
+	
+})(jQuery)
